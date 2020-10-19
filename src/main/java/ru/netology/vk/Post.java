77c4153d994e0141b1d9fe8d8b2c0d7b3dae1db5
Elena <mymoskvina@gmail.com>
2020-10-19 23:13:30 +0300

    Updated

--- src/main/java/ru/netology/vk/Post.java
+++ src/main/java/ru/netology/vk/Post.java
@@ -4,22 +4,22 @@ public class Post {
     private int id; //id поста
     private int userId; // аналогично from_id
     private int ownerId; // добавлено в vk // id пользователя, на чьей стене опубликована запись
-    private int created_by; // добавлено в vk // для сообществ, id администратора, который опубликовал, возвращается не позже 24 часов
+    private int createdBy; // добавлено в vk // для сообществ, id администратора, который опубликовал, возвращается не позже 24 часов
     private String userAvatar;
     private String header;
     private String publicationTime; // аталог date
     private String text; // переименовано как в vk
-    private int reply_owner_id; // добавлено c vk; идентификатор владельца записи, в ответ на которую была оставлена текущая.
-    private int reply_post_id; // добавлено c vk; идентификатор записи, в ответ на которую была оставлена текущая.
-    private boolean friends_only; // добавлено c vk; у них int
+    private int replyOwnerId; // добавлено c vk; идентификатор владельца записи, в ответ на которую была оставлена текущая.
+    private int replyPostId; // добавлено c vk; идентификатор записи, в ответ на которую была оставлена текущая.
+    private boolean friendsOnly; // добавлено c vk; у них int
     private int copyright; // добавлено c vk;
-    private int post_type; // добавлено c vk;
+    private int postType; // добавлено c vk;
     private String postUrl; //нет такого поля в vk, вероятно они это реализуют на фронте
     private boolean interesting; // флаг для того чтобы отображать в выборке "Сначала интересные"
     private String imageUrl; // в vk вынесено в массив прикрепляемых файлов разных типов attachments
     private int countView; // в vk отдельный объект
     private int countShare; // в vk отдельный объект
-    private String countLike; // в vk вынесено в отдельный объект
+    private LikesPost likesPost;
     private CommentsInfo commentsInfo; // аналог объекта comments
 
     public int getId() {
@@ -102,14 +102,6 @@ public class Post {
         this.countShare = countShare;
     }
 
-    public String getCountLike() {
-        return countLike;
-    }
-
-    public void setCountLike(String countLike) {
-        this.countLike = countLike;
-    }
-
     public int getUserId() {
         return userId;
     }
@@ -134,36 +126,36 @@ public class Post {
         this.ownerId = ownerId;
     }
 
-    public int getCreated_by() {
-        return created_by;
+    public int getCreatedBy() {
+        return createdBy;
     }
 
-    public void setCreated_by(int created_by) {
-        this.created_by = created_by;
+    public void setCreated_by(int createdBy) {
+        this.createdBy = createdBy;
     }
 
-    public int getReply_owner_id() {
-        return reply_owner_id;
+    public int getReplyOwnerId() {
+        return replyOwnerId;
     }
 
-    public void setReply_owner_id(int reply_owner_id) {
-        this.reply_owner_id = reply_owner_id;
+    public void setReplyOwnerId(int replyOwnerId) {
+        this.replyOwnerId = replyOwnerId;
     }
 
-    public int getReply_post_id() {
-        return reply_post_id;
+    public int getReplyPostId() {
+        return replyPostId;
     }
 
-    public void setReply_post_id(int reply_post_id) {
-        this.reply_post_id = reply_post_id;
+    public void setReplyPostId(int replyPostId) {
+        this.replyPostId = replyPostId;
     }
 
-    public boolean isFriends_only() {
-        return friends_only;
+    public boolean isFriendsOnly() {
+        return friendsOnly;
     }
 
-    public void setFriends_only(boolean friends_only) {
-        this.friends_only = friends_only;
+    public void setFriendsOnly(boolean friendsOnly) {
+        this.friendsOnly = friendsOnly;
     }
 
     public int getCopyright() {
@@ -174,11 +166,11 @@ public class Post {
         this.copyright = copyright;
     }
 
-    public int getPost_type() {
-        return post_type;
+    public int getPostType() {
+        return postType;
     }
 
-    public void setPost_type(int post_type) {
-        this.post_type = post_type;
+    public void setPostType(int postType) {
+        this.postType = postType;
     }
 }
--- src/main/java/ru/netology/vk/PostManager.java
+++ src/main/java/ru/netology/vk/PostManager.java
@@ -3,7 +3,7 @@ package ru.netology.vk;
 public class PostManager {
     private Post[] post;
 
-    public Post[] search(int postId, String postQuery) {
+    public Post[] search(int postId, String domain, String postQuery, boolean owners_only, int count, int offset, boolean extended) {
         //TODO: add logic
         return null;
     }
