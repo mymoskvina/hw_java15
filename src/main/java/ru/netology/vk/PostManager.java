e7e8f6df2b4b2ba2355241e4bb0effb3a929adfe
Elena <mymoskvina@gmail.com>
2020-10-19 17:32:48 +0300

    Add manager

---
+++ src/main/java/ru/netology/vk/PostManager.java
@@ -0,0 +1,13 @@
+package ru.netology.vk;
+
+public class PostManager {
+    private Post[] post;
+
+    public Post[] search(int postId, String postQuery) {
+        //TODO: add logic
+        return null;
+    }
+    public void delete(int postId, int ownerWallId) {
+        //TODO: add logic
+    }
+}
